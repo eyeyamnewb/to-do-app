acfcdece5148f4b65e07d5956a23ea97fb32514a
eyeyamnewb <166309564+eyeyamnewb@users.noreply.github.com>
2025-03-14 12:37:25 +0800

    Update models.py

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Task(models.Model):
 	title = models.CharField(max_length=200)
 	complete = models.BooleanField(default=False)
-	created = models.DateTimeField(auto_now_add=True)
+	duedate = models.DateTimeField(default=13/10/2011)
 
 	def __str__(self):
 		return self.title
